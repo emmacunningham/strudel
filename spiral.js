8032f39f07838eb758d79f34ab86bd0b5d5d7950
emmacunningham <the.cunning.ham@gmail.com>
2015-02-20 11:46:46 -0800

    more garbage

--- spiral.js
+++ spiral.js
@@ -1,6 +1,6 @@
 // a spiral for john hunter, creator of matplotlib
 
-var width = 400,
+/*var width = 400,
     height = 430
     num_axes = 8,
     tick_axis = 1,
@@ -10,20 +10,20 @@ var width = 400,
 var theta = function(r) {
   return -2*Math.PI*r;
 };
-
+*/
 /*var arc = d3.svg.arc()
   .startAngle(0)
   .endAngle(2*Math.PI);*/
 
-var radius = d3.scale.linear()
+/*var radius = d3.scale.linear()
   .domain([start, end])
-  .range([0, d3.min([width,height])/2-20]);
+  .range([0, d3.min([width,height])/2-20]);*/
 
 /*var angle = d3.scale.linear()
   .domain([0,num_axes])
   .range([0,360])*/
 
-var svg = d3.select("#chart").append("svg")
+/*var svg = d3.select("#chart").append("svg")
     .attr("width", width)
     .attr("height", height)
   .append("g")
@@ -34,7 +34,7 @@ var pieces = d3.range(start, end+0.001, (end-start)/1000);
 var spiral = d3.svg.line.radial()
   .interpolate("cardinal")
   .angle(theta)
-  .radius(radius);
+  .radius(radius);*/
 
 /*svg.append("text")
   .text("And there was much rejoicing!")
@@ -63,7 +63,7 @@ var spiral = d3.svg.line.radial()
     .attr("text-anchor", "middle")
     .attr("transform", function(d) { return "rotate(" + -90 + ")" })*/
 
-svg.selectAll(".spiral")
+/*svg.selectAll(".spiral")
     .data([pieces])
   .enter().append("path")
     .attr("class", "spiral")
@@ -83,7 +83,35 @@ function radial_tick(selection) {
       .attr("text-anchor", "bottom")
       .attr("transform", "rotate(" + angle(axis_num) + ")")
   });
-}
+}*/
+
+var data = d3.range(0, 12 * Math.PI, .01).map(function(t) {
+  return [t, 6*(Math.exp((t*(.5))/(2*Math.PI))-1)/(Math.exp(.5*6)-1)];
+});
+
+
+var width = 960,
+    height = 500,
+    radius = Math.min(width, height) / 2 - 30;
+
+var r = d3.scale.linear()
+    .domain([0, 6])
+    .range([0, radius]);
+
+var line = d3.svg.line.radial()
+    .radius(function(d) { return r(d[1]); })
+    .angle(function(d) { return d[0] + Math.PI / 2; });
+
+var svg = d3.select("body").append("svg")
+    .attr("width", width)
+    .attr("height", height)
+  .append("g")
+    .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
+
+svg.append("path")
+    .datum(data)
+    .attr("class", "line")
+    .attr("d", line);
 
 d3.select("#nRadius").on("input", function() {
   update(+this.value);
@@ -105,8 +133,13 @@ function update(nRadius) {
     .attr("r", nRadius);
   */
 
-  end = nRadius;
+  //end = nRadius;
 
+  var newData = d3.range(0, 12 * Math.PI, .01).map(function(t) {
+    return [t, 6*(Math.exp((t*(nRadius))/(2*Math.PI))-1)/(Math.exp(.5*6)-1)];
+  });
+
+/*
   radius = d3.scale.linear()
     .domain([start, end])
     .range([0, d3.min([width,height])/2-20]);
@@ -118,9 +151,10 @@ function update(nRadius) {
     .angle(theta)
     .radius(radius);
 
+
 svg.selectAll(".spiral")
     .data([pieces]).attr("d", spiral)
 
-
   console.log(nRadius)
+*/
 }
